7b9b30f6dd30bf01ffe5fa5d2ea3b3fb01470ed0
Elmersperry <Elmersperry@mail.ru>
2025-07-01 19:59:32 +0300

    bhk,ihuk

---
+++ top_academy/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Главная страница',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'Главная страница',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    }
+    return render(request, 'main/index.html', context)
